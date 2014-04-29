4a74e2058aa5862783c3b54b4c1a7e038f7c742a
Tarcio <tarcio@gmail.com>
2014-04-29 20:33:41 +1000

    Fixing variable not defined properly

--- routes/test.js
+++ routes/test.js
@@ -10,25 +10,25 @@ router.get('/test', function (req, res) {
     mySpiDevice = '/dev/spidev0.0';
 
   // connecting to SPI
-  myLedStripe.connect(numLEDs, myStripeType, mySpiDevice);
+  ledstripe.connect(numLEDs, myStripeType, mySpiDevice);
 
   // do some fancy stuff
-  myLedStripe.fill(0xFF, 0x00, 0x00);
+  ledstripe.fill(0xFF, 0x00, 0x00);
   console.log("red");
   setTimeout(function () {
-    myLedStripe.fill(0x00, 0xFF, 0x00);
+    ledstripe.fill(0x00, 0xFF, 0x00);
     console.log("green")
   }, 1000);
   setTimeout(function () {
-    myLedStripe.fill(0x00, 0x00, 0xFF);
+    ledstripe.fill(0x00, 0x00, 0xFF);
     console.log("blue")
   }, 2000);
   setTimeout(function () {
-    myLedStripe.fill(0xFF, 0xFF, 0xFF);
+    ledstripe.fill(0xFF, 0xFF, 0xFF);
     console.log("white")
   }, 3000);
   setTimeout(function () {
-    myLedStripe.disconnect();
+    ledstripe.disconnect();
     process.exit()
   }, 4000);
 
