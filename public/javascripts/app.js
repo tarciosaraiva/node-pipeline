ae04b688d8b229fdb28e4e8d1f5533b8208bc610
Tarcio <tarcio@gmail.com>
2014-05-06 13:47:07 +1000

    Reorganizing some files in the directory structure

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,26 +1,3 @@
 'use strict';
 
-var app = angular.module('pipeline', ['ngResource', 'flow']);
-
-$(function () {
-  [{
-    name: '.colorpicker-s',
-    defaultColor: '#00ff00'
-  }, {
-    name: '.colorpicker-f',
-    defaultColor: '#ff0000'
-  }, {
-    name: '.colorpicker-a',
-    defaultColor: '#ffffff'
-  }, {
-    name: '.colorpicker-u',
-    defaultColor: '#ffff00'
-  }, {
-    name: '.colorpicker-anim',
-    defaultColor: '#0000ff'
-  }].forEach(function (el) {
-    $(el.name).colorpicker({
-      color: el.defaultColor
-    });
-  });
-});
+var app = angular.module('pipeline', ['ngResource', 'flow', 'colorpicker.module']);
