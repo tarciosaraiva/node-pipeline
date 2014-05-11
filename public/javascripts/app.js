536301be2c2067d27218e658f5fcb2471ef70308
Tarcio <tarcio@gmail.com>
2014-05-12 09:37:47 +1000

    Now uploading files! \o/

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,13 @@
 'use strict';
 
 var app = angular.module('pipeline', ['ngResource', 'flow', 'colorpicker.module']);
+app.config(['flowFactoryProvider',
+  function (flowFactoryProvider) {
+    flowFactoryProvider.defaults = {
+      target: '/api/sounds/upload',
+      permanentErrors: [404, 500, 501],
+      testChunks: false,
+      simultaneousUploads: 1
+    };
+  }
+]);
--- routes/sounds.js
+++ routes/sounds.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var config = require('./../lib/config');
+var multiparty = require('multiparty');
+var fs = require('fs');
 
 /* GET sounds configuration */
 router.get('/', function (req, res) {
@@ -12,7 +14,30 @@ router.put('/', function (req, res) {
   var payload = req.body;
   config.merge('sounds', payload);
   config.save(function (err) {
-    if (err) res.send(500);
+    if (err) {
+      res.send(500);
+    }
+  });
+  res.send();
+});
+
+// test the chunk
+router.get('/upload', function (req, res) {
+  res.json();
+});
+
+router.post('/upload', function (req, res, next) {
+  var form = new multiparty.Form();
+  form.parse(req);
+  form.on('part', function (part) {
+    if (part.filename) {
+      part.pipe(fs.createWriteStream('./uploads/file', {
+        flags: 'a'
+      }));
+    }
+  });
+  form.on('error', function (err) {
+    if (err) console.log(err);
   });
   res.send();
 });
