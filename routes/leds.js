5468cc21c59951aa6000d2ef60fd48689a7ebb85
Tarcio <tarcio@gmail.com>
2014-05-17 11:37:16 +1000

    LEDs route using strict context and API improved

--- routes/leds.js
+++ routes/leds.js
@@ -1,39 +1,27 @@
+'use strict';
+
 var express = require('express');
 var ledstrip = require('./../lib/ledstrip');
 var router = express.Router();
 var config = require('./../lib/config');
 
-function hexToRgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
-}
-
 router.post('/test', function (req, res) {
-  var start = Number(req.param('start')),
+  var len = Number(req.param('length')),
+    start = Number(req.param('start')),
     end = Number(req.param('end')),
-    length = Number(req.param('length'));
+    animation = req.param('animation'),
+    speed = Number(req.param('speed')),
+    colour = req.param('colour');
 
   ledstrip.connect(length);
-  ledstrip.test(start, end);
-
-  res.send();
-});
-
-router.post('/animate', function (req, res) {
-  var building = req.param('building'),
-    length = Number(req.param('length'));
-
-  ledstrip.connect(length);
-  ledstrip.animate(
-    building.animation,
-    new Buffer(length * 3), {
-      speed: Number(building.speed),
-      colour: hexToRgb(building.colour)
-    });
+  ledstrip.test({
+    length: len,
+    start: start,
+    end: end,
+    animation: animation,
+    speed: speed,
+    colour: colour
+  });
 
   res.send();
 });
