ab0418a15762edc2bf2d73c22d4567a6d0664606
Tarcio <tarcio@gmail.com>
2014-05-20 20:39:49 +1000

    Removing cookie parser as it's not needed

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -23,7 +22,6 @@ app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
